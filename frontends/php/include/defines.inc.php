e5969f1805ac6990b4c44d2d106cf3a10ed8cabe
Mikhail Makurov <makurov@gmail.com>
2019-06-27 18:32:38 +0000

    Update frontends/php/include/defines.inc.php

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 
 define('GLABER_VERSION', '0.1.0');
-define('ZABBIX_VERSION',		'4.0.8');
+define('ZABBIX_VERSION',		'4.0.8rc1');
 define('ZABBIX_API_VERSION',	'4.0.8');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
