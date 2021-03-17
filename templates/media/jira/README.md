bce4adbc731fb18881a7569406889fe93d0e89eb
Mikhail Grigorev <sleuthhound@gmail.com>
2021-03-17 11:06:24 +0500

    Syncing Zabbix original source to Glabel

---
+++ templates/media/jira/README.md
@@ -0,0 +1,56 @@
+# Jira webhook
+
+This guide describes how to integrate Zabbix 5.0 installation with Jira using the Zabbix webhook feature. This guide provides instructions on setting up a media type, a user and an action in Zabbix.<br>
+Please note that recovery and update operations and Jira's custom fields are supported only for trigger-based events.
+
+
+## Setting up webhook in Zabbix 
+1\. Before setting up a Jira Webhook, it is recommended to setup the global macro `{$ZABBIX.URL}` containing an URL to the Zabbix frontend.<br>
+As an example, this macro can be used to populate Jira's custom field with URL to an event info or graph.  
+
+[![](images/thumb.1.png?raw=true)](images/1.png)
+
+2\. In the *Administration > Media types* section, import the [media_jira.xml](media_jira.xml).
+
+3\. Open the newly added **Jira** media type and replace all *&lt;PLACEHOLDERS&gt;* with your values.<br>
+The following parameters are required:<br>
+**jira_url** - actual URL of your Jira instance,<br>
+**jira_user** - Jira user login,<br>
+**jira_password** - password or API token (for Jira Cloud installations an API token can be obtained at https://id.atlassian.com/manage/api-tokens),<br>
+**jira_project_key** - text key of the Jira project (not to be mistaken with an ID!),<br>
+**jira_issue_type** - name of the issue type to be used when creating new issues from Zabbix notifications.<br>
+
+4\. You can customize Jira issue by export information into Jira custom fields.<br>
+By default the webhook does not use Jira custom fields and `{ALERT.MESSAGE}` is used as an issue description.<br>
+
+To export information into your Jira custom field, add a parameter with custom field ID as key (if you need help finding custom field ID, see [this page](https://confluence.atlassian.com/jirakb/how-to-find-id-for-custom-field-s-744522503.html) in Jira documentation). If at least one parameter starting with "customfield_" is configured for the media type, the webhook will use `{TRIGGER.DESCRIPTION}` as an issue description instead of `{ALERT.MESSAGE}` to avoid duplication of information in description and custom fields.<br>
+We recommend creating custom fields for all of `{ALERT.MESSAGE}` fields like "Host", "Link to event info", "Trigger ID", "Event date", etc.
+[![](images/thumb.3.png?raw=true)](images/3.png)<br>
+
+Jira webhook supports the following custom fields types:
+- String
+- URL
+- Number
+- Datetime
+- Single-select
+- Multi-select
+- Checkbox
+- Radio button
+
+*Examples:*<br>
+[![](images/2.png?raw=true)](images/2.png)<br>
+You can use combined parameter value as `{$ZABBIX.URL}/tr_events.php?triggerid={TRIGGER.ID}&eventid={EVENT.ID}` or `{EVENT.DATE}T{EVENT.TIME}`.
+
+Note, that you can pass only one value for each custom field including multi-select and checkbox.<br>
+
+Date and time must be in ISO 8601 format with timezone of the Zabbix server (2020-01-01T23:59:59+0200).
+If server time is set to UTC, there is no need to add timezone at the end of parameter value.
+
+5\. Create a **Zabbix user** and add **Media** with the **Jira** media type. 
+Though a "Send to" field is not used in Jira webhook, it cannot be empty. To comply with frontend requirements, you can put any symbol there.
+Make sure this user has access to all hosts for which you would like problem notifications to be converted into Jira tasks.
+
+For more information see [Zabbix](https://www.zabbix.com/documentation/current/manual/config/notifications) and [Jira](https://support.atlassian.com/jira-software-cloud/) documentations.
+
+## Supported Versions
+Zabbix 5.0
---
+++ ui/tests/unit/include/classes/parsers/CIPParserTest.php
@@ -0,0 +1,319 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2021 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * Class containing tests for CIPParser class functionality.
+ */
+class CIPParserTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * An array of trigger functions and parsed results.
+	 */
+	public static function testProvider() {
+		return [
+			[
+				'192.168.3.4', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '192.168.3.4'
+				]
+			],
+			[
+				'192.168.3.4,192.168.5.0', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '192.168.3.4'
+				]
+			],
+			[
+				'192.168.3.4,192.168.5.0', 12, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '192.168.5.0'
+				]
+			],
+			[
+				'192.168.3.4,192.168.5.0/24', 12, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '192.168.5.0'
+				]
+			],
+			[
+				'192.168.3.256', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'192.168.3.256', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'192.168..4', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'::', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '::'
+				]
+			],
+			[
+				'::', 0, ['v6' => false],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'::1', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '::1'
+				]
+			],
+			[
+				'0000:0000:0000:0000:0000:0000:0000:0001', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '0000:0000:0000:0000:0000:0000:0000:0001'
+				]
+			],
+			[
+				'0000:0000:0000:0000:0000:0000:FFFF:FFFF', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '0000:0000:0000:0000:0000:0000:FFFF:FFFF'
+				]
+			],
+			[
+				'0000:0000:0000:0000:0000:0000:127.0.0.1', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '0000:0000:0000:0000:0000:0000:127.0.0.1'
+				]
+			],
+			[
+				'::FFFF:127.0.0.1', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '::FFFF:127.0.0.1'
+				]
+			],
+			[
+				'1234:5678:90ab:cdef:ABCD:EF00:127.0.0.1', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '1234:5678:90ab:cdef:ABCD:EF00:127.0.0.1'
+				]
+			],
+			[
+				'random text.....1234:5678:90ab:cdef:ABCD:EF00:127.0.0.1....text', 16, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '1234:5678:90ab:cdef:ABCD:EF00:127.0.0.1'
+				]
+			],
+			[
+				'::127.0.0.1', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '::127.0.0.1'
+				]
+			],
+			[
+				'FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:255.255.255.255', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => 'FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:255.255.255.255'
+				]
+			],
+			[
+				'0000:0000:0000:0000:0000:0000:127.0.0.256', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'0000:0000:0000:0000:0000:0000:FFFG:FFFF', 0, ['v6' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'2001:db8::8a2e:370:7334', 0, ['v6' => false],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'{$MACRO}', 0, ['v6' => true, 'usermacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{$MACRO}'
+				]
+			],
+			[
+				'text{$MACRO}', 4, ['v6' => true, 'usermacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{$MACRO}'
+				]
+			],
+			[
+				'{$MACRO}text', 0, ['v6' => true, 'usermacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '{$MACRO}'
+				]
+			],
+			[
+				'{$MACRO:"test"}', 0, ['v6' => true, 'usermacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{$MACRO:"test"}'
+				]
+			],
+			[
+				'{#MACRO}', 0, ['v6' => true, 'usermacros' => true],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'{#MACRO}', 0, ['v6' => true, 'lldmacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{#MACRO}'
+				]
+			],
+			[
+				'{{#M}.regsub("^([0-9]+)", "{#M}: \1")}', 0, ['lldmacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{{#M}.regsub("^([0-9]+)", "{#M}: \1")}'
+				]
+			],
+			[
+				'{{#M}.regsub("^([0-9]+)", "{#M}: \1")}', 0, [],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'test{#MACRO}', 4, ['v6' => true, 'lldmacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{#MACRO}'
+				]
+			],
+			[
+				'{#MACRO}test', 0, ['v6' => true, 'lldmacros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '{#MACRO}'
+				]
+			],
+			[
+				'{HOST.HOST}', 0, ['v6' => true, 'usermacros' => true, 'lldmacros' => true, 'macros' => true],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{HOST.HOST}'
+				]
+			],
+			[
+				'{HOST.HOST}', 0, ['v6' => true, 'macros' => []],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'{HOST.HOST}', 0, ['v6' => true, 'macros' => false],
+				[
+					'rc' => CParser::PARSE_FAIL,
+					'match' => ''
+				]
+			],
+			[
+				'{HOST.HOST}', 0, ['v6' => true, 'macros' => ['{HOST.NAME}', '{HOST.HOST}', '{HOST.DNS}']],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{HOST.HOST}'
+				]
+			],
+			[
+				'test{HOST.HOST}', 4, ['v6' => true, 'macros' => ['{HOST.NAME}', '{HOST.HOST}', '{HOST.DNS}']],
+				[
+					'rc' => CParser::PARSE_SUCCESS,
+					'match' => '{HOST.HOST}'
+				]
+			],
+			[
+				'{HOST.HOST}test', 0, ['v6' => true, 'macros' => ['{HOST.NAME}', '{HOST.HOST}', '{HOST.DNS}']],
+				[
+					'rc' => CParser::PARSE_SUCCESS_CONT,
+					'match' => '{HOST.HOST}'
+				]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider testProvider
+	 *
+	 * @param string $source
+	 * @param int    $pos
+	 * @param array  $options
+	 * @param array  $expected
+	*/
+	public function testParse($source, $pos, $options, $expected) {
+		$ip_parser = new CIPParser($options);
+
+		$this->assertSame($expected, [
+			'rc' => $ip_parser->parse($source, $pos),
+			'match' => $ip_parser->getMatch()
+		]);
+		$this->assertSame(strlen($expected['match']), $ip_parser->getLength());
+	}
+
+}
