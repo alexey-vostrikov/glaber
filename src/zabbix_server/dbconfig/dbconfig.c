aa0ca7c3b21dc01bb22082e70f456823f4912c24
root <makurov@gmail.com>
2022-07-05 17:09:57 +0000

    added config sync logging to find constant sync load

--- src/zabbix_server/dbconfig/dbconfig.c
+++ src/zabbix_server/dbconfig/dbconfig.c
@@ -116,7 +116,8 @@ ZBX_THREAD_ENTRY(dbconfig_thread, args)
 		sec = time(NULL) - sec;
 		zbx_update_env(sec);
 
-		LOG_DBG("SYNC: Full resync in %d, DC_ConfigNeedsSync() = %d, secrets_reload = %d", nextcheck-time(NULL), DC_ConfigNeedsSync(), secrets_reload);
+		LOG_INF("SYNC: Full resync in %d, DC_ConfigNeedsSync() = %d, secrets_reload = %d", nextcheck-time(NULL), 
+						DC_ConfigNeedsSync(), secrets_reload);
 		while (nextcheck > time(NULL) &&  FAIL == DC_ConfigNeedsSync() &&  0 == secrets_reload) 
 		{
 
@@ -126,7 +127,7 @@ ZBX_THREAD_ENTRY(dbconfig_thread, args)
 			cset_time = changeset_get_recent_time();
 			 
 			 if ( cset_time > 0 && (time(NULL) - cset_time > CHANGESET_AUTOLOAD_TIME )) {
-			//	LOG_INF("auto load changeset data");
+				LOG_INF("auto load changeset data");
 				DC_RequestConfigSync();
 			}
 			
@@ -147,11 +148,13 @@ ZBX_THREAD_ENTRY(dbconfig_thread, args)
 			int sync_type = GLB_DBSYNC_CHANGESET;
 			
 			if (nextcheck < time(NULL)) {
-			//	LOG_INF("It's time for fill config SYNC");
+				LOG_INF("Doing full SYNC");
 				//sync_type = ZBX_DBSYNC_UPDATE;
 				sync_type = ZBX_DBSYNC_INIT;
 				nextcheck = time(NULL) + CONFIG_CONFSYNCER_FREQUENCY;
-			} 
+			} else {
+				LOG_INF("Doing diff SYNC");
+			}
 
 			DCsync_configuration(sync_type);
 			DCupdate_interfaces_availability();
