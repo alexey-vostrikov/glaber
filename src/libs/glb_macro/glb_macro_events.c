8caed8344e24f4f1cdb8f174acb50d35879f5caa
root <makurov@gmail.com>
2023-02-21 16:44:44 +0500

    started refactoring macro translation

--- src/libs/glb_macro/glb_macro_events.c
+++ src/libs/glb_macro/glb_macro_events.c
@@ -26,88 +26,24 @@ having data needed */
 #include "zbxserver.h"
 
 
+convert_dc_trigger_2_event(ZBX_DB_EVENT *event, DC_TRIGGER *trigger) {
+	
+}
+
 int glb_macro_translate_event_name(DC_TRIGGER *trigger, char *result, int max_len) {
+	char err[MAX_STRING_LEN];
+		
+	//	zbx_substitute_simple_macros(NULL, trigger, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	//								 &event->trigger.correlation_tag, MACRO_TYPE_TRIGGER_TAG, err, sizeof(err));
 
-    ZBX_DB_EVENT event={0};
-    char err[MAX_STRING_LEN];
-    
-    event.eventid = 0;
-	//event.history_idx = history_idx;
-	event.source = EVENT_SOURCE_TRIGGERS;
-	event.object = EVENT_OBJECT_TRIGGER;
-	event.objectid = trigger->triggerid;
-	event.name = NULL;
-	event.clock = time(NULL);
-	event.ns = 0;
-	event->value = trigger->new_value;
-	event->acknowledged = EVENT_NOT_ACKNOWLEDGED;
-	event->flags = ZBX_FLAGS_DB_EVENT_CREATE;
-	event->severity = TRIGGER_SEVERITY_NOT_CLASSIFIED;
-	event->suppressed = ZBX_PROBLEM_SUPPRESSED_FALSE;
+	ZBX_DB_EVENT event={0};
+	convert_dc_trigger_2_event(&event, trigger);
 
-	if (EVENT_SOURCE_TRIGGERS == source)
-	{
-		char err[256];
-		zbx_dc_um_handle_t *um_handle;
+	zbx_substitute_simple_macros(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+			 &trigger->event_name, MACRO_TYPE_EVENT_NAME, err, sizeof(err));
 
-		um_handle = zbx_dc_open_user_macros();
+    free_event(&event);
 
-		if (TRIGGER_VALUE_PROBLEM == value)
-			event->severity = trigger_priority;
-
-		event->trigger.triggerid = objectid;
-		event->trigger.description = zbx_strdup(NULL, trigger_description);
-		event->trigger.expression = zbx_strdup(NULL, trigger_expression);
-		event->trigger.recovery_expression = zbx_strdup(NULL, trigger_recovery_expression);
-		event->trigger.priority = trigger_priority;
-		event->trigger.type = trigger_type;
-		event->trigger.correlation_mode = trigger_correlation_mode;
-		event->trigger.correlation_tag = zbx_strdup(NULL, trigger_correlation_tag);
-		event->trigger.value = trigger_value;
-		event->trigger.opdata = zbx_strdup(NULL, trigger_opdata);
-		event->trigger.event_name = ((NULL != event_name && event_name[0] != '\0') ? zbx_strdup(NULL, event_name) : NULL);
-		event->name = zbx_strdup(NULL, ((NULL != event_name && event_name[0] != '\0') ? event_name : trigger_description));
-		event->trigger.cache = NULL;
-		event->trigger.url = NULL;
-		event->trigger.url_name = NULL;
-		event->trigger.comments = NULL;
-
-		zbx_substitute_simple_macros(NULL, event, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-									 &event->trigger.correlation_tag, MACRO_TYPE_TRIGGER_TAG, err, sizeof(err));
-
-		zbx_substitute_simple_macros(NULL, event, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-									 &event->name, MACRO_TYPE_EVENT_NAME, err, sizeof(err));
-
-		zbx_vector_ptr_create(&event->tags);
-
-		if (NULL != trigger_tags)
-		{
-			for (i = 0; i < trigger_tags->values_num; i++)
-				process_trigger_tag(event, (const zbx_tag_t *)trigger_tags->values[i]);
-		}
-
-		zbx_vector_ptr_create(&item_tags);
-		get_item_tags_by_expression(&event->trigger, &item_tags);
-
-		for (i = 0; i < item_tags.values_num; i++)
-		{
-			process_item_tag(event, (const zbx_item_tag_t *)item_tags.values[i]);
-			zbx_free_item_tag(item_tags.values[i]);
-		}
-
-		zbx_vector_ptr_destroy(&item_tags);
-
-		zbx_dc_close_user_macros(um_handle);
-	}
-
-
-
-
-
-
-    zbx_substitute_simple_macros(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-    					 &event.name, MACRO_TYPE_EVENT_NAME, err, sizeof(err));
-     
 	LOG_INF("Standalone version of translate: translat");
 	HALT_HERE("Not implemented");
 }
