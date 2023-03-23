83713eb36325a72166aad02741d503c11338f621
root <makurov@gmail.com>
2023-03-23 12:30:38 +0500

    removed active checks annoying message

--- src/zabbix_server/trapper/active.c
+++ src/zabbix_server/trapper/active.c
@@ -351,8 +351,8 @@ int	send_list_of_active_checks(zbx_socket_t *sock, char *request, int config_tim
 
 	zbx_free(buffer);
 out:
-	if (FAIL == ret)
-		zabbix_log(LOG_LEVEL_WARNING, "cannot send list of active checks to \"%s\": %s", sock->peer, error);
+	//if (FAIL == ret)
+	//	DEBUG_ITEM( "cannot send list of active checks to \"%s\": %s", sock->peer, error);
 
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __func__, zbx_result_string(ret));
 
@@ -714,7 +714,7 @@ int	send_list_of_active_checks_json(zbx_socket_t *sock, struct zbx_json_parse *j
 
 	goto out;
 error:
-	zabbix_log(LOG_LEVEL_WARNING, "cannot send list of active checks to \"%s\": %s", sock->peer, error);
+	//zabbix_log(LOG_LEVEL_WARNING, "cannot send list of active checks to \"%s\": %s", sock->peer, error);
 
 	zbx_json_init(&json, ZBX_JSON_STAT_BUF_LEN);
 	zbx_json_addstring(&json, ZBX_PROTO_TAG_RESPONSE, ZBX_PROTO_VALUE_FAILED, ZBX_JSON_TYPE_STRING);
