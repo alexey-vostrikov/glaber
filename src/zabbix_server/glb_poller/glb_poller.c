2879a370e9885b2e90cc0779f4a26789adf8726d
root <makurov@gmail.com>
2023-03-26 12:02:58 +0500

    fix: typo

--- src/zabbix_server/glb_poller/glb_poller.c
+++ src/zabbix_server/glb_poller/glb_poller.c
@@ -800,7 +800,7 @@ ZBX_THREAD_ENTRY(glbpoller_thread, args)
 			conf.procinfo.server_num, get_process_type_string(conf.procinfo.process_type), conf.procinfo.process_num);
 
 	zbx_setproctitle("%s #%d [started]", get_process_type_string(conf.procinfo.process_type), conf.procinfo.process_num);
-c
+
 	poller_async_loop_run();
 
 	poll_shutdown(&conf);
