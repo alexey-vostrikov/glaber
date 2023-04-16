743c09a193a17121defcdde2cef0c62dc5016e81
miramir <gmiramir@gmail.com>
2023-04-16 16:55:00 +0500

    Добавляет массовое создание связей между узлами

--- ui/include/classes/api/services/CHostDepends.php
+++ ui/include/classes/api/services/CHostDepends.php
@@ -5,6 +5,7 @@ class CHostDepends extends CApiService
     public const ACCESS_RULES = [
         'get' => ['min_user_type' => USER_TYPE_ZABBIX_USER],
         'create' => ['min_user_type' => USER_TYPE_ZABBIX_ADMIN],
+        'masscreate' => ['min_user_type' => USER_TYPE_ZABBIX_ADMIN],
         'update' => ['min_user_type' => USER_TYPE_ZABBIX_ADMIN],
         'delete' => ['min_user_type' => USER_TYPE_ZABBIX_ADMIN]
     ];
@@ -50,12 +51,18 @@ class CHostDepends extends CApiService
         }
 
         if ($options['with_hostname']) {
-            $sqlParts['from']['host_up'] = 'hosts h_up';
-            $sqlParts['where']['host_up'] = 'h_up.hostid=hd.hostid_up';
+            $sqlParts['left_join_ex'][] = [
+                'table' => 'hosts',
+                'alias' => 'h_up',
+                'on' => [$this->tableAlias.'.hostid_up' => 'h_up.hostid']
+            ];
             $sqlParts['select'][] = 'h_up.name as hostname_up';
 
-            $sqlParts['from']['host_down'] = 'hosts h_down';
-            $sqlParts['where']['host_down'] = 'h_down.hostid=hd.hostid_down';
+            $sqlParts['left_join_ex'][] = [
+                'table' => 'hosts',
+                'alias' => 'h_down',
+                'on' => [$this->tableAlias.'.hostid_down' => 'h_down.hostid'],
+            ];
             $sqlParts['select'][] = 'h_down.name as hostname_down';
         }
 
@@ -209,5 +216,82 @@ class CHostDepends extends CApiService
 
     private function validateGet(array $options)
     {
+    }
+
+
+    /**
+     * @param array $deps
+     * @param int $deps[]['hostid_up']
+     * @param int $deps[]['hostid_down']
+     * @param string $deps[]['name']
+     * @return array
+     * @throws APIException
+     */
+    public function masscreate($deps)
+    {
+        $this->validateMassCreate($deps);
+
+        $hostIds = array_unique(
+            array_column($deps, 'hostid_up')
+            +array_column($deps, 'hostid_down')
+        );
+
+        $this->checkPermissions($hostIds, _('You do not have permission to perform this operation.'));
+
+        $depids = DB::insert($this->tableName, $deps);
+
+        if (empty($depids)) {
+            self::exception(ZBX_API_ERROR_INTERNAL, _s('Error on insert'));
+        }
+
+        return ['depids' => $depids];
+    }
+
+    private function validateMassCreate(array $deps)
+    {
+        foreach ($deps as $dep) {
+            // required fields
+            if (!array_key_exists('hostid_up', $dep) || !array_key_exists('hostid_down', $dep)) {
+                self::exception(ZBX_API_ERROR_PARAMETERS, _s('Fields "hostid_up" and "hostid_down" is required'));
+            }
+
+            // field types
+            $rules = ['type' => API_OBJECT, 'fields' => [
+                'hostid_up' => ['type' => API_UINT64],
+                'hostid_down' => ['type' => API_UINT64],
+                'name' => ['type' => API_STRINGS_UTF8, 'flags' => API_NORMALIZE, 'length' => 128],
+            ]];
+
+            $error = '';
+
+            if (!CApiInputValidator::validate($rules, $dep, '/', $error)) {
+                self::exception(ZBX_API_ERROR_PARAMETERS, $error);
+            }
+        }
+    }
+
+
+    /**
+     * Checks if all of the given hosts are available for writing.
+     *
+     * @throws APIException     if a host is not writable or does not exist
+     *
+     * @param array  $hostids
+     * @param string $error
+     */
+    protected function checkPermissions(array $hostids, $error) {
+        if ($hostids) {
+            $hostids = array_unique($hostids);
+
+            $count = API::Host()->get([
+                'countOutput' => true,
+                'hostids' => $hostids,
+                'editable' => true
+            ]);
+
+            if ($count != count($hostids)) {
+                self::exception(ZBX_API_ERROR_PERMISSIONS, $error);
+            }
+        }
     }
 }
