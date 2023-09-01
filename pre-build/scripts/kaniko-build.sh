57bd7874d234e9d3c3ff721a4ca6010f58c1ae09
Nikolay Lebedev <fifo.mail@gmail.com>
2023-09-01 22:12:16 +0200

    : kaniko build refactoring, pre-build

--- pre-build/scripts/kaniko-build.sh
+++ pre-build/scripts/kaniko-build.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
+# https://semver.org/spec/v2.0.0.html
 
 # variables
-GITLAB_API_URL="https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/"
+GITLAB_API_URL="https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/registry/repositories"
 SEARCH_PATH="${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/${CI_JOB_NAME}"
+JQ_URL="https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"
 
 # Install jq
-wget -q --no-check-certificate -O /busybox/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+wget -q --no-check-certificate -O /busybox/jq ${JQ_URL}
 chmod +x /busybox/jq
 
 # Send GET request to GitLab API to retrieve repository details
-REPO_DETAILS=$(wget --no-check-certificate -q -O - "${GITLAB_API_URL}" | jq --arg path "${SEARCH_PATH}" '.[] | select(.path == $path)')
+REPO_DETAILS=$(wget --no-check-certificate -q -O - "${GITLAB_API_URL}" | \
+               jq --arg path "${SEARCH_PATH}" '.[] | select(.path == $path)')
 
 # Extract repository ID from repository details
 REPO_ID=$(echo "${REPO_DETAILS}" | jq -r '.id')
 
-# Build image if not equal the starting version 1.0.1
-# https://semver.org/spec/v2.0.0.html
-
-if [ -z "$REPO_ID" ] ; then
-  echo "Build and push ${CI_JOB_NAME}:${BUILD_TAG} with kaniko: "
+# Make kaniko build function
+kaniko_build() {
   /kaniko/executor --context "${BUILD_DIR}" \
                    --build-arg OS=${OS} \
                    --build-arg OS_VER=${OS_VER} \
@@ -27,26 +27,22 @@ if [ -z "$REPO_ID" ] ; then
                    --cache=true \
                    --cache-repo="${CACHE_REPO}" \
                    --cleanup
-elif [ -z "$REPO_ID" ] && [ "$BUILD_TAG" != "1.0.1" ] ; then
-  echo "Wrong docker image name ${CI_JOB_NAME}, exiting"
-  exit 1
+}
+
+# Build image if docker repository not exist
+if [ -z "$REPO_ID" ] ; then
+  echo "Build and push ${CI_JOB_NAME}:${BUILD_TAG} with kaniko"
+  kaniko_build
 else
   # Set image repository URL
-  IMAGE_REPO_URL="https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${REPO_ID}"
+  IMAGE_REPO_URL="${GITLAB_API_URL}/${REPO_ID}"
 
   # Check if docker image already exists
   if [[ "$(wget --no-check-certificate -q -O - "${IMAGE_REPO_URL}/tags/${BUILD_TAG}" | jq -r '.name')" == "${BUILD_TAG}" ]]
   then
     echo "Docker image ${CI_JOB_NAME}:${BUILD_TAG} already exists, skipping the build"
   else
-    echo "Build and push ${CI_JOB_NAME}:${BUILD_TAG} with kaniko: "
-    /kaniko/executor --context "${BUILD_DIR}" \
-                    --build-arg OS=${OS} \
-                    --build-arg OS_VER=${OS_VER} \
-                    --dockerfile "${BUILD_DIR}/Dockerfile" \
-                    --destination "${BUILD_IMG}:${BUILD_TAG}" \
-                    --cache=true \
-                    --cache-repo="${CACHE_REPO}" \
-                    --cleanup
+    echo "Build and push ${CI_JOB_NAME}:${BUILD_TAG} with kaniko"
+    kaniko_build
   fi
 fi
